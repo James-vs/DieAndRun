cb1b867b6475f1695985ebae7907906e2956b029
mccaw12 <winstonmccarthy15@gmail.com>
2022-11-17 18:30:57 +0000

    Aesthetic impovements + key and gate added to the game

--- Assets/Scripts/playercontroller.cs
+++ Assets/Scripts/playercontroller.cs
@@ -9,7 +9,7 @@ private float movementX;
 private float movementY;
 public float speed = 0;
 public float jumpamount = 0;
-public bool isGrounded = true;
+public bool isGrounded = false;
 
     // Start is called before the first frame update
 void Start() { rb = GetComponent<Rigidbody2D>(); }
@@ -20,37 +20,50 @@ void OnMove(InputValue movementValue){
     movementY = movementVector.y;
     }
 
+
+
+
 void FixedUpdate(){
     Vector3 horizontalmovement = new Vector3(movementX,0);
     Vector4 verticalmovement = new Vector4(0,movementY);
     rb.AddForce(horizontalmovement * speed);
-    if(isGrounded & movementY > 0){
-       Debug.Log("run");
+    if(movementX != 0){}
+    if(isGrounded){
        if(movementX != 0){
         rb.AddForce(verticalmovement * jumpamount * 1.5f, ForceMode2D.Impulse);
        }
        else{
        rb.AddForce(verticalmovement * jumpamount, ForceMode2D.Impulse);}
-      }
+      } 
 
       }
+
 
     
 
  
 
   void OnCollisionEnter2D(Collision2D other){
+    Debug.Log(other.gameObject.tag);
 
 
     if(other.gameObject.tag == "Ground"){
     isGrounded = true;}
+    else{isGrounded = false;}
+    if(other.gameObject.tag == "Hazards"){
+      Destroy(gameObject);
+    }
+  
     
       
   }
   
   void OnCollisionExit2D(Collision2D other){
-    isGrounded = false;
-    Debug.Log("Is Grounded = " + isGrounded);
+    if(other.gameObject.tag == "Ground"){
+    isGrounded = false;}
+    else{
+      isGrounded = true;
+    }
        }
 
 }      
